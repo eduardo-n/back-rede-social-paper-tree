b46beee3752b81321907b4dd1f25e4a255c0b147
Eduardo Nunes <edununes726@gmail.com>
2022-01-31 22:40:35 -0300

    projeto criado

---
+++ backend/src/main/java/com/projetotcc/papertree/PapertreeApplication.java
@@ -0,0 +1,13 @@
+package com.projetotcc.papertree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapertreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PapertreeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/projetotcc/papertree/PapertreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetotcc.papertree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapertreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
