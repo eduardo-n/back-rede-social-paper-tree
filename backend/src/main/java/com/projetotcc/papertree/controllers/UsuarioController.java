8f66396c8a63f7e05aecd552f213403f400b5dc5
Eduardo Nunes <edununes726@gmail.com>
2022-02-21 23:08:03 -0300

    incrementando endpoint para login com email e senha

--- backend/src/main/java/com/projetotcc/papertree/controllers/UsuarioController.java
+++ backend/src/main/java/com/projetotcc/papertree/controllers/UsuarioController.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projetotcc.papertree.dto.UsuarioDTO;
+import com.projetotcc.papertree.entities.Usuario;
 import com.projetotcc.papertree.services.UsuarioService;
 
 @RestController
@@ -22,5 +24,10 @@ public class UsuarioController {
 	public ResponseEntity<List<UsuarioDTO>> findAll(){
 		List<UsuarioDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping("/login/{email}/{senha}")
+	public Boolean findUsersWithEmailAndPassword(@PathVariable("email") String email, @PathVariable("senha") String senha){
+		return service.findUsersWithEmailAndPassword(email, senha);
 	}
 }
--- backend/src/main/java/com/projetotcc/papertree/services/UsuarioService.java
+++ backend/src/main/java/com/projetotcc/papertree/services/UsuarioService.java
@@ -3,6 +3,8 @@ package com.projetotcc.papertree.services;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import java.util.Objects;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,5 +23,16 @@ public class UsuarioService {
 	public List<UsuarioDTO> findAll(){
 		List<Usuario> list = repository.findAll();
 		return list.stream().map(x -> new UsuarioDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public Boolean findUsersWithEmailAndPassword(String email, String senha){
+		Usuario usuario = new Usuario();
+		usuario = (Usuario) repository.findUsersWithEmailAndPassword(email, senha);
+		
+		if(Objects.isNull(usuario)) {
+			return false;
+		}
+		return true;
 	}
 }
