6f9393b7ef3ea0631b798c157ef40556c61b9489
victorpcastro <victor.castro@itsgroup.com.br>
2022-05-15 19:51:52 -0300

    inserir trabalhos

--- backend/src/main/java/com/projetotcc/papertree/controllers/TrabalhoController.java
+++ backend/src/main/java/com/projetotcc/papertree/controllers/TrabalhoController.java
@@ -1,15 +1,20 @@
 package com.projetotcc.papertree.controllers;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.projetotcc.papertree.dto.TrabalhoDTO;
+import com.projetotcc.papertree.dto.UsuarioDTO;
 import com.projetotcc.papertree.entities.Usuario;
 import com.projetotcc.papertree.services.TrabalhoService;
 
@@ -40,8 +45,18 @@ public class TrabalhoController {
 		*/
 		
 		
-		List<TrabalhoDTO> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<TrabalhoDTO> listTrabalho = service.findAll();
+		return ResponseEntity.ok().body(listTrabalho);
+	}
+	
+	@PostMapping("/inserir")
+	public ResponseEntity<TrabalhoDTO> insert(@RequestBody TrabalhoDTO dto){
+		
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+		
 	}
 	
 }
