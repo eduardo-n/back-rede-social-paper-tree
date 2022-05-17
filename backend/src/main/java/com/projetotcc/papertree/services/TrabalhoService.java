53ddadae85b3cbbcfd910b299540bed93fc26eea
Victor Paiva de Castro <51804123+victorpcastro@users.noreply.github.com>
2022-05-17 01:01:21 -0300

    Revert "inserir trabalhos"

--- backend/src/main/java/com/projetotcc/papertree/services/TrabalhoService.java
+++ backend/src/main/java/com/projetotcc/papertree/services/TrabalhoService.java
@@ -8,11 +8,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.projetotcc.papertree.dto.TrabalhoDTO;
-import com.projetotcc.papertree.dto.UsuarioDTO;
 import com.projetotcc.papertree.entities.Trabalho;
-import com.projetotcc.papertree.entities.Usuario;
 import com.projetotcc.papertree.repositories.TrabalhoRepository;
-import com.projetotcc.papertree.repositories.UsuarioRepository;
 
 @Service
 public class TrabalhoService {
@@ -20,27 +17,9 @@ public class TrabalhoService {
 	@Autowired
 	private TrabalhoRepository repository;
 	
-	@Autowired
-	private UsuarioRepository usuarioRepository;
-	
 	@Transactional
 	public List<TrabalhoDTO> findAll(){
 		List<Trabalho> list = repository.findAll();
 		return list.stream().map(x -> new TrabalhoDTO(x)).collect(Collectors.toList());
-	}
-	
-	@Transactional
-	public TrabalhoDTO insert(TrabalhoDTO dto){
-		
-		Trabalho trabalho = new Trabalho(null, dto.getTitulo(), dto.getArea(), dto.getDescricao());
-		
-		for(UsuarioDTO contribuidores : dto.getContribuidores()) {
-			Usuario usuario = usuarioRepository.getOne(contribuidores.getId());
-			trabalho.getContribuidores().add(usuario);
-		}
-		
-		trabalho = repository.save(trabalho);
-		return new TrabalhoDTO(trabalho);
-		
 	}
 }
